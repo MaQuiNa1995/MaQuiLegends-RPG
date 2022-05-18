24886e104a367f8be25433de588359479fb51eb8
MaQuiNa1995 <maquina1995@gmail.com>
2022-05-18 23:41:21 +0200

    Añadidas clases y configuración básica de arquetipo

---
+++ src/test/java/com/github/maquina1995/maquilegends/RigorousTest.java
@@ -0,0 +1,15 @@
+package com.github.maquina1995.maquilegends;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigorousTest {
+
+	@Test
+	void rigorousTest() {
+		Assertions.assertTrue(true);
+	}
+
+}
